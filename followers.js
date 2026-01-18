17ad8faba26b7c2eca261c62e0cb27c117253f4c
Afolabi Boluwatife <afolabieniola95@gmail.com>
2026-01-18 21:27:58 +0100

    working in progress

---
+++ followers.js
@@ -0,0 +1,7 @@
+const followersContainer = document.querySelector('.followers-container');
+
+followersContainer.textContent = "No followers yet";
+followersContainer.style.textAlign = "center";
+followersContainer.style.color = "grey";
+followersContainer.style.paddingTop = "50px";
+followersContainer.style.fontSize = "17px";
