31ee4c24f9fb80edd51d1d96b376dc54f23cac2c
Afolabi Boluwatife <afolabieniola95@gmail.com>
2026-01-18 01:38:16 +0100

    working in progress...

--- create-posts.js
+++ create-posts.js
@@ -51,6 +51,9 @@ const effect = document.getElementById('effect');
 const effectPanel = document.getElementById('effect-panel');
 const effectPanelClose = document.getElementById('effect-panel-close');
 const mediaNav = document.getElementById('media-nav');
+const music = document.getElementById('music');
+const musicPanel = document.getElementById('music-panel');
+
 
 addText.addEventListener('click', () =>{
   const file = fileInput.files?.[0];
@@ -107,13 +110,30 @@ effectPanel.style.height = '0px';
 effectPanelClose.style.display = 'none';
 });
 
+
+music.addEventListener('click', ()=>{
+const file = fileInput.files?.[0];
+  if(!file){
+    btnAlert.style.display = 'block';
+    setTimeout(()=>{
+   btnAlert.style.display = 'none';
+  }, 2000);
+    return;
+  }else{
+  musicPanel.style.height = '100vh';
+  }
+});
+
+const dragHandle = document.querySelector('.drag-handle');
+dragHandle.addEventListener('click', ()=>{
+musicPanel.style.height = '0px';
+});
+
 const popular = document.getElementById('popular');
-const trendy = document.getElementById('trendy');
-const sound = document.getElementById('sound');
+const trending = document.getElementById('trending');
 const storage = document.getElementById('storage');
 const popularContainer = document.getElementById('popular-container');
-const trendyContainer = document.getElementById('trendy-container');
-const soundContainer = document.getElementById('sound-container');
+const trendingContainer = document.getElementById('trending-container');
 const storageContainer = document.getElementById('storage-container');
 
 
@@ -126,69 +146,35 @@ popular.addEventListener('click', ()=>{
   popular.style.color = 'white';
   popular.style.textShadow = '1px 1px white';
 
-  trendy.style.background = 'white';
-  trendy.style.color = 'black';
-  trendy.style.textShadow = '1px 1px white';
-
-  sound.style.background = 'white';
-  sound.style.color = 'black';
-  sound.style.textShadow = '1px 1px white';
+  trending.style.background = 'white';
+  trending.style.color = 'black';
+  trending.style.textShadow = '1px 1px white';
 
   storage.style.background = 'white';
   storage.style.color = 'black';
   storage.style.textShadow = '1px 1px white';
 
   popularContainer.style.display = 'block';
-  trendyContainer.style.display = 'none';
-  soundContainer.style.display = 'none';
+  trendingContainer.style.display = 'none';
   storageContainer.style.display = 'none';
 
 });
 
-trendy.addEventListener('click', ()=>{
-  trendy.style.background = 'black';
-  trendy.style.color = 'white';
-  trendy.style.textShadow = '1px 1px white';
+trending.addEventListener('click', ()=>{
+  trending.style.background = 'black';
+  trending.style.color = 'white';
+  trending.style.textShadow = '1px 1px white';
 
   popular.style.background = 'white';
   popular.style.color = 'black';
   popular.style.textShadow = '1px 1px white';
-
-  sound.style.background = 'white';
-  sound.style.color = 'black';
-  sound.style.textShadow = '1px 1px white';
 
   storage.style.background = 'white';
   storage.style.color = 'black';
   storage.style.textShadow = '1px 1px white';
 
   popularContainer.style.display = 'none';
-  trendyContainer.style.display = 'block';
-  soundContainer.style.display = 'none';
-  storageContainer.style.display = 'none';
-
-});
-
-sound.addEventListener('click', ()=>{
-  sound.style.background = 'black';
-  sound.style.color = 'white';
-  sound.style.textShadow = '1px 1px white';
-
-  popular.style.background = 'white';
-  popular.style.color = 'black';
-  popular.style.textShadow = '1px 1px white';
-
-  trendy.style.background = 'white';
-  trendy.style.color = 'black';
-  trendy.style.textShadow = '1px 1px white';
-
-  storage.style.background = 'white';
-  storage.style.color = 'black';
-  storage.style.textShadow = '1px 1px white';
-
-  popularContainer.style.display = 'none';
-  trendyContainer.style.display = 'none';
-  soundContainer.style.display = 'block';
+  trendingContainer.style.display = 'block';
   storageContainer.style.display = 'none';
 
 });
@@ -202,17 +188,12 @@ storage.addEventListener('click', ()=>{
   popular.style.color = 'black';
   popular.style.textShadow = '1px 1px white';
 
-  trendy.style.background = 'white';
-  trendy.style.color = 'black';
-  trendy.style.textShadow = '1px 1px white';
-
-  sound.style.background = 'white';
-  sound.style.color = 'black';
-  sound.style.textShadow = '1px 1px white';
+  trending.style.background = 'white';
+  trending.style.color = 'black';
+  trending.style.textShadow = '1px 1px white';
 
   popularContainer.style.display = 'none';
-  trendyContainer.style.display = 'none';
-  soundContainer.style.display = 'none';
+  trendingContainer.style.display = 'none';
   storageContainer.style.display = 'block';
 
 });
