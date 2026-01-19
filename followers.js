98e5da0c1d8190e0960e4deb965adc482331894c
afolabieniola95 <afolabieniola95@gmail.com>
2026-01-19 20:25:56 +0100

    update followers.js

--- followers.js
+++ followers.js
@@ -1,7 +1,18 @@
 const followersContainer = document.querySelector('.followers-container');
 
-followersContainer.textContent = "No followers yet";
+followersContainer.textontent = "No followers yet";
 followersContainer.style.textAlign = "center";
 followersContainer.style.color = "grey";
 followersContainer.style.paddingTop = "50px";
 followersContainer.style.fontSize = "17px";
+
+const followBtn = document.querySelector('.follow-btn');
+const dots = document.querySelector('.dot');
+
+followBtn.addEventListener('click', ()=>{
+  followBtn.style.display = 'none';
+  dots.style.display = 'block';
+});
+
+const dots = document.createElement('div');
+dots.className = 'dot';
