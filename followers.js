c1b57623c3d20ff2d6b775f089d876195ad27866
afolabieniola95 <afolabieniola95@gmail.com>
2026-01-20 12:07:19 +0100

    update followers.js

--- followers.js
+++ followers.js
@@ -1,33 +1,40 @@
 
 
 const followAlert = document.querySelector('.follow-alert');
-const dotMenu = document.querySelector('.dot-menu');
-const dot = document.querySelector('.dots');
 const menuClose = document.querySelector('.dot-menu-close');
+const menu = document.querySelector('.menu-modal');
 
-dot.addEventListener('click', ()=>{
-dotMenu.style.top = '5%';
-dotMenu.style.right = '2%';
-dotMenu.style.width = '100px';
-dotMenu.style.height = '80px';
-
-menuClose.style.display = 'block';
-});
 
 menuClose.addEventListener('click', ()=>{
-dotMenu.style.top = '0%';
-dotMenu.style.right = '0%';
-dotMenu.style.width = '0px';
-dotMenu.style.height = '0px';
+menu.style.top = '0%';
+menu.style.right = '0%';
+menu.style.width = '0px';
+menu.style.height = '0px';
 
 menuClose.style.display = 'none';
+});
+
+document.addEventListener('click', (e)=>{
+  if(e.target.classList.contains('menu-dot')){
+    const card = e.target.closest('.follower-card');
+    const menuDot = card.querySelector('.menu-dot');
+    const menu = document.querySelector('.menu-modal');
+    const menuClose = document.querySelector('.dot-menu-close');
+   
+   
+   menu.style.top = '5%';
+   menu.style.right = '2%'
+   menu.style.width = '100px';
+   menu.style.height = '80px';
+   menuClose.style.display = 'block';
+  }
 });
 
 document.addEventListener('click', (e)=>{
   if(e.target.classList.contains('follow-btn')){
     const card = e.target.closest('.follower-card');
     const followBtn = card.querySelector('.follow-btn');
-    const dots = card.querySelector('.dots');
+    const dots = card.querySelector('.menu-dot');
 
     followBtn.style.display = 'none';
     dots.style.display = 'flex';
